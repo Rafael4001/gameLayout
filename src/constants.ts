efed3fb5652c2759f0ff8d1a782da22c212940fa
Rafał Ciesielczuk <rafal.ciesielczuk@gmail.com>
2020-04-29 23:13:17 +0200

    step

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,11 @@
 import happyCare from './images/happyCare.svg'
+import fortuneCookie from './images/fortuneCookie.svg'
+import fortuneCookie_2 from './images/fortuneCookie_2.svg'
+import userAvatar from './images/userAvatar.svg'
+import gun_220fx130f from './images/gun_220fx130f.png'
 
 export const HAPPY_CARE = happyCare;
+export const FORTUNE_COOKIE = fortuneCookie;
+export const FORTUNE_COOKIE_2 = fortuneCookie_2;
+export const USER_AVATAR = userAvatar;
+export const GUN_1 = gun_220fx130f;
--- src/views/Homepage/Homepage.component.tsx
+++ src/views/Homepage/Homepage.component.tsx
@@ -3,7 +3,13 @@ import React from 'react';
 
 import styles from './Homepage.module.scss';
 
-import { HAPPY_CARE } from '../../constants'
+import {
+  HAPPY_CARE,
+  FORTUNE_COOKIE,
+  FORTUNE_COOKIE_2,
+  USER_AVATAR,
+  GUN_1,
+} from '../../constants'
 
 const VAR_1 = 756;
 
@@ -12,10 +18,19 @@ const Homepage = () => {
   return (
     <div className={styles.mainContainer}>
 
-      <div className={styles.userStatus}>ONLINE: <span className={styles.userStatusAmount}>{VAR_1}</span></div>
+      <div className={styles.userStatus}>
+        <img
+          src={USER_AVATAR}
+          alt={"HAPPY_CARE"}
+          className={styles.iconContainerFORTUNE_COOKIE}
+        />
+        <span> ONLINE:</span>
+        <span className={styles.userStatusAmount}>{VAR_1}</span>
+      </div>
 
       <div className={styles.buttonsContainer}>
-        <div className={styles.rhombus}>
+
+        <div className={styles.rhombus_1}>
           <img
             src={HAPPY_CARE}
             alt={"HAPPY_CARE"}
@@ -23,15 +38,32 @@ const Homepage = () => {
           />
           <div>ODBIERZ DZIENNY BONUS</div>
         </div>
-        <div className={styles.rhombus}>
+
+        <div className={styles.rhombus_2}>
           <img
-            src={HAPPY_CARE}
+            src={FORTUNE_COOKIE}
             alt={"HAPPY_CARE"}
-            className={styles.iconContainer}
+            className={styles.iconContainerFORTUNE_COOKIE}
           />
-          <div> LOTERIA W CIEMNO</div>
+          <div>LOTERIA W CIEMNO</div>
+          <img
+            src={FORTUNE_COOKIE_2}
+            alt={"HAPPY_CARE"}
+            className={styles.iconContainerFORTUNE_COOKIE_2}
+          />
+
         </div>
-        <div className={styles.rhombus}>TEXT 3</div>
+
+        <div className={styles.rhombus_3}>
+          <span className={styles.text_1}>BEST DROP: </span>
+          <span>SAFARI MESH</span>
+          <img
+            src={GUN_1}
+            alt={"HAPPY_CARE"}
+            className={styles.iconContainerFORTUNE_COOKIE_2}
+          />
+        </div>
+
       </div>
 
 
